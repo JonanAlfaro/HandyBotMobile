1554040c037381a1eef580a3cc80e7cd97ac6a58
ZoroarkXZ6 <emmgh@gmail.com>
2024-08-07 19:16:58 -0600

    Cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
 
-// import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 // import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { provideHttpClient , HttpClientModule} from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { provideHttpClient , HttpClientModule} from '@angular/common/http';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { url: 'http://192.168.100.251:12345', options: {} };
 
 
 
-// @NgModule({
-//   declarations: [AppComponent],
-//   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
-//   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy,
-//   },provideHttpClient()],
-//   bootstrap: [AppComponent],
-// })
-// export class AppModule {}
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(),HttpClientModule, SocketIoModule.forRoot(config),],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy,
+  },provideHttpClient()],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+
+
